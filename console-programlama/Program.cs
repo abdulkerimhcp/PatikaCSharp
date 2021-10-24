0c5e5101a3dc520798f26eedef0a0c3150b9821d
Abdulkerim Hıcıp <abdulkerimhcp@gmail.com>
2021-10-24 16:36:16 +0300

    first commit

---
+++ console-programlama/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace console_programlama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("İsim Giriniz:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Soyisim Giriniz:");
+            string surname = Console.ReadLine();
+
+            Console.WriteLine("Merhaba " + name + " " + surname);
+        }
+    }
+}
